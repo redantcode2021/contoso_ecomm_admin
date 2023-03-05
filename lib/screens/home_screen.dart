30d5739b1c61030a4339a54eda845982a3dbfd8f
redantcode2021 <redantcyber@gmail.com>
2023-03-05 16:24:48 +0700

    1-admin-panel-product-screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:contoco_ecom_admin/screens/product_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contoso E-comm Admin Panel'),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: 150,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: InkWell(
+          onTap: () {
+            Get.to(() => ProductScreen());
+          },
+          child: Card(
+            child: Center(
+              child: Text('Go To Products'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
